1c5a8f84b9ae8ea91bc3c561d819fbfa7d7a158c
nishizaki0456 <44010816+nishizaki0456@users.noreply.github.com>
2018-10-11 16:41:16 +0900

    Update block.js

--- repository/block.js
+++ repository/block.js
@@ -17,7 +17,7 @@
         var connection = new WebSocket('ws://192.168.250.110:30002');
 
         connection.onopen = function () {
-            connection.send(toBytes("movej([0,-1.57, 0,"+ str(0) +","+  str(PI/2) + ","+ str(PI/2)+ " ], a=1.39 , v=1.04)" + "\n")); 
+            connection.send("movej([0,-1.57, 0,"+ str(0) +","+  str(PI/2) + ","+ str(PI/2)+ " ], a=1.39 , v=1.04)" + "\n"); 
         };
         wait = Math.random();
         console.log('Waiting for ' + wait + ' seconds');
