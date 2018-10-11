541911ae7a6dd96653a97e305e87ed4fac792bec
nishizaki0456 <naoya6540@gmail.com>
2018-10-11 14:26:43 +0900

    ソケット通信1

---
+++ repository/websocket1.js
@@ -0,0 +1,56 @@
+var WebSocketServer = require('ws').Server;
+var spawn = require('child_process').spawn;
+var wss = new WebSocketServer({port: 80});
+
+wss.on('connection', function(ws) {
+    console.log('connection');
+
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        var msg = JSON.parse(message);
+
+	if(msg.command == 'eject') {
+            var child = spawn("powershell.exe",
+                ["function Eject-CDROM {\n" +
+                 "$wmplayer = New-Object -ComObject WMPlayer.OCX \n" +
+                 "$wmplayer.cdromCollection.Item(0).Eject() \n" +
+                 "} \n" +
+                 "Eject-CDROM\n"]);
+            child.on("close", function(error) {
+                console.log('exec eject: %s', error);
+                var data = {status: 'ejected', error: error };
+                ws.send(JSON.stringify(data));
+            });
+	}
+        else if(msg.command == 'close') {
+            var child = spawn("powershell.exe",
+                ["function Eject-CDROM {\n" +
+                 "$wmplayer = New-Object -ComObject WMPlayer.OCX \n" +
+                 "$wmplayer.cdromCollection.Item(0).Eject() \n" +
+                 "$wmplayer.cdromCollection.Item(0).Eject() \n" +
+                 "} \n" +
+                 "Eject-CDROM\n"]);
+
+            child.on("close", function(error) {
+                console.log('exec close: %s', error);
+                var data = {status: 'closed', error: error };
+                ws.send(JSON.stringify(data));
+            });
+        }
+    });
+
+    ws.on('error', function(err) {
+        console.log('error: %s', error);
+    });
+});
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+
+    wss.close();
+
+    process.exit();
+});
+
+
+
